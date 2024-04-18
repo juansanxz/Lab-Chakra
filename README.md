758fcb2ca1b8dd62af82606d73a3f6df69901beb
Juan Sánchez <juan.sanchez-pe@mail.escuelaing.edu.co>
2024-04-18 17:33:10 -0500

    Lab chakra

---
+++ README.md
@@ -0,0 +1,1 @@
+![Paisaje](image.png)
---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import { Button, Flex, Heading } from '@chakra-ui/react'
+import './App.css'
+import labImage from './img/lab.jpeg';
+
+function App() {
+
+  const bgImage = `url(${labImage})`;
+
+  return (
+    <>
+      <Flex height="100vh" alignItems="center" justifyContent="center">
+      <Flex direction="column" bgImage={bgImage} bgSize="cover" bgPosition="center" p={200} rounded={6}>
+        <Heading mb={6} marginTop={-150} color="white" fontWeight="normal" fontFamily="arial">DOMINA EL TERRENO</Heading>
+        <Flex justifyContent="space-between" width="100%">
+        <Button 
+              variant="outline" 
+              color="white" 
+              fontFamily="arial" 
+              marginBottom="20px" // Agrega un margen inferior para bajar los botones
+              _hover={{ color: 'black', bg: 'white' }} // Cambia el color del texto al pasar el cursor
+            >
+              VER DETALLES
+            </Button>
+            <Button 
+              variant="outline" 
+              color="white" 
+              fontFamily="arial" 
+              marginBottom="20px" // Agrega un margen inferior para bajar los botones
+              _hover={{ color: 'black', bg: 'white' }} // Cambia el color del texto al pasar el cursor
+            >
+              VER VIDEO
+            </Button>
+        </Flex>
+      </Flex>
+    </Flex>
+    </>
+  )
+}
+
+export default App
